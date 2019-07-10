a99684f7e1918d54b881ca9d8b79af0949b7225f
Amrit Baveja <abaveja313@gmail.com>
2019-07-09 18:28:13 -0700

    DBAAS-2573: Removed version import

--- splicemachinesa/__init__.py
+++ splicemachinesa/__init__.py
@@ -1,10 +1,3 @@
 from . import splice_machine, pyodbc, base
-from ..setup import VERSION
-
-__VERSION__ = VERSION  # alias
-
-###################
-# Package Version #
-###################
 
 base.dialect = splice_machine.dialect
