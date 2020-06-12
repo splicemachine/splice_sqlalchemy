d65d07905f6d7eeabf7cbb7a4d199aa57026ba4c
Ben Epstein <ben.epstein97@gmail.com>
2020-06-11 21:56:47 -0400

    setup

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = '0.1.4'
+VERSION = '0.1.4.1'
 
 setup(
     name='splicemachinesa',
