d4047a2c5e71a5c26dfa9d5a6a2b9f364204da30
Ben Epstein <ben.epstein97@gmail.com>
2020-08-18 20:01:45 -0400

    bump version for pr

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-VERSION = '0.1.7.1'
+VERSION = '0.1.9'
 ODBC_VERSION = '2.8.66.0'
 
 def bash(command):
